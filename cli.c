e391bf64669b86e3c714cbc9b71b1dbb412bb83a
Savindra Kumar <45175425+SavindraKumar@users.noreply.github.com>
2019-03-10 18:36:05 +0530

    Revert "2 refactor code"

--- cli.c
+++ cli.c
@@ -1,17 +1,17 @@
-//! @addtogroup Cli
-//! @brief Cli Interface
-//! @{
-//
-//****************************************************************************
-//! @file cli.c
-//! @brief Cli interface
-//! @author Savindra Kumar(savindran1989@gmail.com)
-//! @bug No known bugs.
-//
-//****************************************************************************
-//****************************************************************************
-//                           Includes
-//****************************************************************************
+/** @addtogroup Cli
+ *  @brief Cli Interface
+ *  @{
+ */
+/*****************************************************************************/
+/** @file cli.c
+ *  @brief Cli interface
+ *  @author Savindra Kumar(savindran1989@gmail.com)
+ *  @bug No known bugs.
+ */
+/*****************************************************************************/
+/******************************************************************************
+ *                           Includes
+ ******************************************************************************/
 //standard header files
 #include <stdio.h>
 #include <string.h>
@@ -21,239 +21,226 @@
 #include "cli.h"
 #include "app.h"
 
-//****************************************************************************
-//                           Defines and typedefs
-//****************************************************************************
-#define CLI_DEF_TEXT               "\r\nCli-> "
-#define DELIMITER                  ' '
-#define CMD_SIZE_IN_BYTES          100
-#define CMD_OFFSET                   0
-#define PARAM_OFFSET                 1
-#define MAX_NUM_OF_PARAMS            8
+/******************************************************************************
+ *                           Defines and typedefs
+ ******************************************************************************/
+#define COMMAND_SIZE_IN_BYTES      100
+#define COMMAND_START_OFFSET         0
+#define PARAMETER_START_OFFSET       1
 
-//****************************************************************************
-//                           Private Functions
-//****************************************************************************
+/******************************************************************************
+ *                           Private Functions
+ ******************************************************************************/
 static uint8_t RxHandler (char *pcData, uint8_t ucBytesReceived);
 static uint8_t GetParameters (char *pcData, uint8_t ucLength, char cDelimiter, char *ppcTokens[]);
 static uint8_t Help (char **ppcParameters, uint8_t ucParameterCount, char *pcResult);
 
-//****************************************************************************
-//                           external variables
-//****************************************************************************
+/******************************************************************************
+ *                           external variables
+ ******************************************************************************/
 
-//****************************************************************************
-//                           Private variables
-//****************************************************************************
-static const CliCmdList_t m_pCliCmdList[] =
+/******************************************************************************
+ *                           Private variables
+ ******************************************************************************/
+static const CliCommandList_t m_pCliCommandList[] =
 {
-    { "help", Help, "Displays list of supported commands", 0 },
-    { "info", AppShowInfo, "Shows device info", 0 },
-    { "exit", AppExit, "Exit from program", 0 },
-    { "sum", AppSum, "Sum of two numbers", 2 }
+	{"help", Help, "Displays list of supported commands", 0},
+	{"info", AppShowInfo, "Shows device info", 0},
+	{"exit", AppExit, "Exit from program", 0},
+	{"sum", AppSum, "Sum of two numbers", 2},
 };
 
-static char m_pcCmd[CMD_SIZE_IN_BYTES];
-static uint8_t m_ucIndex = 0;
+static char m_pcCommand[COMMAND_SIZE_IN_BYTES];
+static uint8_t m_ucIndex                        = 0;
 
-//****************************************************************************
-//                    G L O B A L  F U N C T I O N S
-//****************************************************************************
-//
-//! @brief Initialise Cli
-//! @param[in]  None
-//! @param[out] pcResult   Pointer to result buffer
-//! @return     None
-//
+
+/******************************************************************************
+ *                    G L O B A L  F U N C T I O N S
+ ******************************************************************************/
+
+/** @brief Initialise Cli
+ *  @param[in]  None
+ *  @param[out] pcResult   Pointer to result buffer
+ *  @return    None
+ */
 void CliInit(char *pcResult)
 {
-    sprintf(pcResult, CLI_DEF_TEXT);
-} //end CliInit
+	sprintf(pcResult, CLI_DEF_TEXT);
+}//end CliInit
 
-//
-//! @brief Process characters received from Cli
-//! @param[in]  pcData          Pointer to characters received
-//! @param[in]  ucBytesRec      Number of characters received
-//! @param[out] pcResult        Pointer to result buffer
-//! @return     uint8_t         true -Command executed successfully,
-//!                             false-Command not executed
-//
-uint8_t CliProcessCmd(char *pcData, uint8_t ucBytesRec, char *pcResult)
+/** @brief Process characters received from Cli
+ *  @param[in]  pcData          Pointer to characters received
+ *  @param[in]  ucBytesReceived Number of characters received
+ *  @param[out] pcResult        Pointer to result buffer
+ *  @return     uint8_t         true-Command executed successfully,
+ *                              false-Command not executed
+ */
+uint8_t CliProcessCommand(char *pcData, uint8_t ucBytesReceived, char *pcResult)
 {
-    char     *ppcParams[MAX_NUM_OF_PARAMS]  = {0};
-    uint8_t  ucParamCount                   = 0;
-    uint8_t  ucCount                        = 0;
-    uint16_t usNumOfCmds                    = 0;
-    bool     bIsCmdRec                      = false;
-    bool     bIsCmdProcess                  = false;
+	char *ppcParameters[8];
+	bool bCommandReceived     = false;
+	bool bReturn              = false;
+	uint8_t ucParameterCount  = 0;
+	uint8_t ucCount           = 0;
 
 
-    bIsCmdRec = RxHandler(pcData, ucBytesRec);
+	bCommandReceived = RxHandler(pcData, ucBytesReceived);
 
-    if ( (true == bIsCmdRec) && (strlen(m_pcCmd) > 0) )
-    {
-        ucParamCount = GetParameters(m_pcCmd, strlen(m_pcCmd), DELIMITER, ppcParams);
-        usNumOfCmds  = sizeof(m_pCliCmdList) / sizeof(m_pCliCmdList[0]);
+	if ((true == bCommandReceived) && (strlen((const char *)m_pcCommand) > 0))
+	{
+		ucParameterCount = GetParameters(m_pcCommand, strlen((const char *)m_pcCommand), ' ', ppcParameters);
 
-        for (ucCount = 0; ucCount < usNumOfCmds; ucCount++)
-        {
-            if (!strcmp(ppcParams[CMD_OFFSET], m_pCliCmdList[ucCount].pcName))
-            {
-                uint8_t ucParamsInCmd = 0;
+		for (ucCount = 0; ucCount < sizeof (m_pCliCommandList) / sizeof (m_pCliCommandList[0]); ucCount++)
+		{
+			if (!strcmp(ppcParameters[COMMAND_START_OFFSET], m_pCliCommandList[ucCount].pcName))
+			{
+				uint8_t ucParametersInCommmand = 0;
 
-                ucParamsInCmd = ucParamCount - 1;
+				ucParametersInCommmand = (ucParameterCount - 1);
 
-                if (ucParamsInCmd == m_pCliCmdList[ucCount].ucExpectedNumOfParams)
-                {
-                    bIsCmdProcess = m_pCliCmdList[ucCount].CliExecuteCmd(&ppcParams[PARAM_OFFSET],
-                                                                         ucParamsInCmd, pcResult);
-                }
-                else
-                {
-                    strcpy(pcResult, "\nIncorrect Parameters\r\n");
-                    bIsCmdProcess = true;
-                }
-                break;
-            } //end if
-        }//end for
+				if( ucParametersInCommmand == m_pCliCommandList[ucCount].ucExpectedNumOfParameters)
+				{
+					bReturn = m_pCliCommandList[ucCount].CliExecuteCommand(&ppcParameters[PARAMETER_START_OFFSET], ucParametersInCommmand, pcResult);
+				}
+				else
+				{
+					strcpy(pcResult, "\nIncorrect Parameters\r\n");
+					bReturn =true;
+				}
+				break;
+			}//end if
+		}//end for
 
-        if (ucCount == usNumOfCmds)
-        {
-            strcpy(pcResult, "\nCommand not found\r\n");
-            bIsCmdProcess = true;
-        }//end if
-    }//end if
+		if (ucCount == sizeof (m_pCliCommandList) / sizeof (m_pCliCommandList[0]))
+		{
+			strcpy(pcResult, "\nCommand not found\r\n");
+			bReturn = true;
+		}//end if
+	}//end if
 
-    return bIsCmdProcess;
-} //end CliProcessCommand
+	return bReturn;
+}//end CliProcessCommand
 
-//
-//! @brief Clear Cli buffer
-//! @param[in]  None
-//! @param[out] pcResult Pointer to command text
-//! @return     None
-//
-void CliResetBuffer (char *pcResult)
+/** @brief Clear Cli buffer
+ *  @param[in]  None
+ *  @param[out] pcResult Pointer to command text
+ *  @return     None
+ *
+ */
+void CliResetBuffer(char *pcResult)
 {
-    m_ucIndex = 0;
-    memset(m_pcCmd, 0, sizeof(m_pcCmd));
-    sprintf(pcResult, CLI_DEF_TEXT);
-} //end CliResetBuffer
+	m_ucIndex          = 0;
+	memset(m_pcCommand, 0, sizeof(m_pcCommand));
+	sprintf(pcResult, CLI_DEF_TEXT);
+}//end CliResetBuffer
 
-//****************************************************************************
-//                           L O C A L  F U N C T I O N S
-//****************************************************************************
-//
-//! @brief Get Parameters from commands
-//! @param[in] pcData     Pointer to command string
-//! @param[in] ucLength   Size of command string
-//! @param[in] cDelimiter Delimiter used to separate parameters
-//! @param[in] ppcTokens  Pointer to number of tokens in command
-//! @return    uint8_t    Number of parameters in command string
-//
-static uint8_t GetParameters(char *pcData, uint8_t ucLength, char cDelimiter, char *ppcTokens[])
+/******************************************************************************
+ *                           L O C A L  F U N C T I O N S
+ ******************************************************************************/
+/** @brief Get Parameters from commands
+ *  @param[in] pcData     Pointer to command string
+ *  @param[in] ucLength   Size of command string
+ *  @param[in] cDelimiter Delimiter used to separate parameters
+ *  @param[in] Parameters Pointer to parameters array of string
+ *  @return    uint8_t    Number of parameters in command string
+ */
+static uint8_t GetParameters(char *pcData, uint8_t ucLength, char cDelimiter, char *ppcParameters[])
 {
-    uint8_t ucParamCount  = 0;
-    bool    bIsSpaceInCmd = false;
+	uint8_t ucParameterCount = 0;
+	uint8_t ucSpace          = false;
 
-    ppcTokens[ucParamCount++] = pcData;
+	ppcParameters[ucParameterCount++] = pcData;
 
-    for (uint8_t ucCount = 0; ucCount < ucLength; ucCount++)
-    {
-        if (*(pcData + ucCount) == cDelimiter)
-        {
-            if (true != bIsSpaceInCmd)
-            {
-                *(pcData + ucCount)     = '\0';
-                ppcTokens[ucParamCount] = &pcData[ucCount + 1];
-                ucParamCount++;
-                bIsSpaceInCmd = true;
-            } //end if
-        } //end if
-        else
-        {
-            bIsSpaceInCmd = false;
-        } //end else
-    } //end for
+	for (uint8_t ucCount = 0; ucCount < ucLength; ucCount++)
+	{
+		if (*(pcData + ucCount) == cDelimiter)
+		{
+			if (true != ucSpace)
+			{
+				*(pcData + ucCount) = '\0';
+				ppcParameters[ucParameterCount] = &pcData[ucCount + 1];
+				ucParameterCount++;
+				ucSpace = true;
+			}//end if
+		}//end if
+		else
+		{
+			ucSpace = false;
+		}//end else
+	}//end for
 
-    if (true == bIsSpaceInCmd)
-    {
-        ucParamCount--;
-    }
+	if (true == ucSpace)
+	{
+		ucParameterCount--;
+	}
 
-    return ucParamCount;
+	return ucParameterCount;
 }//end GetParameters
 
-//
-//! @brief Handles Received characters from Cli
-//! @param[in] pcData           Pointer to Received characters
-//! @param[in] ucBytesRec       Number of character received
-//! @return    uint8_t          true  - Command received
-//!                             false - command not received yet
-//
-static uint8_t RxHandler(char *pcData, uint8_t ucBytesRec)
+/** @brief Handles Received characters from Cli
+ *  @param[in] pcData           Pointer to Received characters
+ *  @param[in] ucParameterCount Number of character received
+ *  @return    uint8_t          true - Command received, false - command not received yet
+ *
+ */
+static uint8_t RxHandler(char *pcData, uint8_t ucBytesReceived)
 {
-    bool bIsCmdRec = false;
+	bool bReturn = false;
 
-    for (uint8_t ucCount = 0; ucCount < ucBytesRec; ucCount++)
-    {
-        //Convert string to lowercase
-        if ((pcData[ucCount] >= 65) && (pcData[ucCount] <= 90))
-        {
-            pcData[ucCount] += 32;
-        }
+	for (uint8_t ucCount = 0; ucCount < ucBytesReceived; ucCount++)
+	{
+		//Convert string to lowercase
+		if ((pcData[ucCount] >= 65) && (pcData[ucCount] <= 90))
+		{
+			pcData[ucCount] += 32;
+		}
 
-        m_pcCmd[m_ucIndex] = pcData[ucCount];
+		m_pcCommand[m_ucIndex] = pcData[ucCount];
 
-        if ('\r' == m_pcCmd[m_ucIndex])
-        {
-            m_pcCmd[m_ucIndex] = '\0';
-            bIsCmdRec          = true;
-        }
-        if ('\b' == m_pcCmd[m_ucIndex])
-        {
-            if (m_ucIndex)
-            {
-                m_ucIndex--;
-            }
-        }
-        else if (m_ucIndex < (sizeof(m_pcCmd) - 1))
-        {
-            m_ucIndex++;
-        }
-        m_pcCmd[m_ucIndex] = '\0';
-    }
+		if ('\r' == m_pcCommand[m_ucIndex])
+		{
+			m_pcCommand[m_ucIndex] = '\0';
+			bReturn = true;
+		}
+		if ('\b' == m_pcCommand[m_ucIndex])
+		{
+			if(m_ucIndex)
+			{
+				m_ucIndex--;
+			}
+		}
+		else if (m_ucIndex < (sizeof(m_pcCommand) - 1))
+		{
+			m_ucIndex++;
+		}
+		m_pcCommand[m_ucIndex] = '\0';
+	}
 
-    return bIsCmdRec;
+	return bReturn;
 }//RxHandler
 
-//
-//! @brief Display available commands
-//! @param[in]  ppcParams      Pointer to parameters string
-//! @param[in]  ucParamCount   Number of Parameters
-//! @param[out] pcResult       Pointer to result buffer
-//! @return     uint8_t        true -Command executed successfully,
-//!                            false-Command not executed
-//
-static uint8_t Help(char **ppcParams, uint8_t ucParamCount, char *pcResult)
+/** @brief Display available commands
+ *  @param[in]  ppcParameters      Pointer to parameters string
+ *  @param[in]  ucParameterCount   Number of Parameters
+ *  @param[out] pcResult           Pointer to result buffer
+ *  @return     uint8_t            true-Command executed successfully,
+ *                                 false-Command not executed
+ */
+static uint8_t Help(char **ppcParameters, uint8_t ucParameterCount, char *pcResult)
 {
-    uint16_t usLengthInBytes = 0;
-    uint16_t usNumOfCmds     = 0;
+	uint16_t usLength = 0;
 
-    usLengthInBytes += sprintf(pcResult + usLengthInBytes, "\r\n");
-    usNumOfCmds      = sizeof(m_pCliCmdList) / sizeof(m_pCliCmdList[0]);
+	usLength += sprintf(pcResult + usLength, "\r\n");
 
-    for (uint8_t ucCount = 0; ucCount < usNumOfCmds; ucCount++)
-    {
-        usLengthInBytes += sprintf(pcResult + usLengthInBytes, "\t%-15s" " - %s\r\n",
-                                   m_pCliCmdList[ucCount].pcName,
-                                   m_pCliCmdList[ucCount].pcDescription);
-    }
+	for (uint8_t ucCount = 0; ucCount < sizeof (m_pCliCommandList) / sizeof (m_pCliCommandList[0]); ucCount++)
+	{
+		usLength += sprintf(pcResult + usLength, "\t%-15s"" - %s\r\n", m_pCliCommandList[ucCount].pcName, m_pCliCommandList[ucCount].pcDescription);
+	}
 
-    return true;
+	return true;
 }//end Help
 
-//****************************************************************************
-//                             End of file
-//****************************************************************************
-//! @}
+/******************************************************************************
+ *                             End of file
+ ******************************************************************************/
+/** @}*/
