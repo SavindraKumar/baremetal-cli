7f2fec16205496b111788e38906f6bbe027dc6d7
Savindra <savindran1989@gmail.com>
2019-05-29 23:36:12 +0530

    Refactor test code

--- tests/test_code/test_cli.cpp
+++ tests/test_code/test_cli.cpp
@@ -4,138 +4,130 @@
 #include <string.h>
 #include <stdio.h>
 
-
 extern "C"
 {
 	#include "app.h"
 	#include "cli.h"
 }
 
-#define RESULT_BUF_SIZE          256
-#define HELP_CMD_SIZE              6 
-#define INFO_CMD_SIZE              6
-#define SUM_CMD_SIZE               9
-#define SUM_CMD_SPACE_SIZE        11
-#define WRONG_CMD_SIZE             7
-#define WRONG_PARAMETERS_CMD_SIZE  5
-
-
+#define RESULT_BUF_SIZE            (256u)
+#define HELP_CMD_SIZE              (6u) 
+#define INFO_CMD_SIZE              (6u)
+#define SUM_CMD_SIZE               (9u)
+#define SUM_CMD_SPACE_SIZE         (11u)
+#define WRONG_CMD_SIZE             (7u)
+#define WRONG_PARAMETERS_CMD_SIZE  (5u)
 
 TEST_GROUP(Module)
 {
-	char *pcResultBuf = NULL;
+    char *pcResult = NULL;
 
     void setup()
-    {	
-	    pcResultBuf = (char*)calloc(RESULT_BUF_SIZE, sizeof(char));
-
+    {
+        pcResult = (char*)calloc(RESULT_BUF_SIZE, sizeof(char));
     }
 
     void teardown()
     {
-		free(pcResultBuf);
+        free(pcResult);
     }
 };
 
 TEST(Module, cli_Init_test)
 {
-    cli_Init(pcResultBuf);
-	STRCMP_EQUAL(CLI_DEF_TEXT, pcResultBuf);
+    cli_Init(pcResult);
+    STRCMP_EQUAL(CLI_DEF_TEXT, pcResult);
 }
 
 TEST(Module, cli_process_cmd_null_test)
 {
-	char *pcInput      = NULL;
-	bool bIsCmdProcess = false;
-	
-	bIsCmdProcess = cli_ProcessCmd(pcInput, 2, pcResultBuf);
-	CHECK_EQUAL(false, bIsCmdProcess);
-}
+    char *pcInput      = NULL;
+    bool bIsCmdProcess = false;
 
+    bIsCmdProcess = cli_ProcessCmd(pcInput, 2, pcResult);
+    CHECK_EQUAL(false, bIsCmdProcess);
+}
 
 TEST(Module, cli_process_cmd_help_test)
 {
-	char pcInput[HELP_CMD_SIZE]  = "help\r";
-	bool bIsCmdProcess           = false;
-	
-	bIsCmdProcess = cli_ProcessCmd(pcInput, HELP_CMD_SIZE, pcResultBuf);
-	printf("%s", pcResultBuf);
-	cli_ResetBuffer(pcResultBuf);
-	printf("%s", pcResultBuf);
-	CHECK_EQUAL(true, bIsCmdProcess);
+    char pcInputBuf[HELP_CMD_SIZE] = "help\r";
+    bool bIsCmdProcess             = false;
+
+    bIsCmdProcess = cli_ProcessCmd(pcInputBuf, HELP_CMD_SIZE, pcResult);
+    printf("%s", pcResult);
+    cli_ResetBuffer(pcResult);
+    printf("%s", pcResult);
+    CHECK_EQUAL(true, bIsCmdProcess);
 }
 
 TEST(Module, cli_process_cmd_info_test)
 {
-	char pcInput[INFO_CMD_SIZE]  = "info\r";
-	bool bIsCmdProcess           = false;
-	
-	bIsCmdProcess = cli_ProcessCmd(pcInput, INFO_CMD_SIZE, pcResultBuf);
-	printf("%s", pcResultBuf);
-	cli_ResetBuffer(pcResultBuf);
-	printf("%s", pcResultBuf);	
-	CHECK_EQUAL(true, bIsCmdProcess);
-}
+    char pcInputBuf[INFO_CMD_SIZE] = "info\r";
+    bool bIsCmdProcess             = false;
 
+    bIsCmdProcess = cli_ProcessCmd(pcInputBuf, INFO_CMD_SIZE, pcResult);
+    printf("%s", pcResult);
+    cli_ResetBuffer(pcResult);
+    printf("%s", pcResult);	
+    CHECK_EQUAL(true, bIsCmdProcess);
+}
 
 TEST(Module, cli_process_cmd_sum_test)
 {
-	char pcInput[SUM_CMD_SIZE]  = "sum 1 2\r";
-	bool bIsCmdProcess          = false;
-	
-	bIsCmdProcess = cli_ProcessCmd(pcInput, SUM_CMD_SIZE, pcResultBuf);
-	printf("%s", pcResultBuf);
-	cli_ResetBuffer(pcResultBuf);
-	printf("%s", pcResultBuf);		
-	CHECK_EQUAL(true, bIsCmdProcess);
-}
+    char pcInputBuf[SUM_CMD_SIZE] = "sum 1 2\r";
+    bool bIsCmdProcess            = false;
 
+    bIsCmdProcess = cli_ProcessCmd(pcInputBuf, SUM_CMD_SIZE, pcResult);
+    printf("%s", pcResult);
+    cli_ResetBuffer(pcResult);
+    printf("%s", pcResult);		
+    CHECK_EQUAL(true, bIsCmdProcess);
+}
 
 TEST(Module, cli_process_cmd_sum_with_space_test)
 {
-	char pcInput[SUM_CMD_SPACE_SIZE]  = "sum  1  2\r";
-	bool bIsCmdProcess                = false;
-	
-	bIsCmdProcess = cli_ProcessCmd(pcInput, SUM_CMD_SPACE_SIZE, pcResultBuf);
-	printf("%s", pcResultBuf);
-	cli_ResetBuffer(pcResultBuf);
-	printf("%s", pcResultBuf);	
-	CHECK_EQUAL(true, bIsCmdProcess);
+    char pcInputBuf[SUM_CMD_SPACE_SIZE] = "sum  1  2\r";
+    bool bIsCmdProcess                  = false;
+
+    bIsCmdProcess = cli_ProcessCmd(pcInputBuf, SUM_CMD_SPACE_SIZE, pcResult);
+    printf("%s", pcResult);
+    cli_ResetBuffer(pcResult);
+    printf("%s", pcResult);	
+    CHECK_EQUAL(true, bIsCmdProcess);
 }
 
 TEST(Module, cli_wrong_cmd_test)
 {
-	char pcInput[WRONG_CMD_SIZE]  = "wrong\r";
-	bool bIsCmdProcess            = false;
-	
-	bIsCmdProcess = cli_ProcessCmd(pcInput, WRONG_CMD_SIZE, pcResultBuf);
-	printf("%s", pcResultBuf);
-	cli_ResetBuffer(pcResultBuf);
-	printf("%s", pcResultBuf);	
-	CHECK_EQUAL(true, bIsCmdProcess);
-}
+    char pcInputBuf[WRONG_CMD_SIZE] = "wrong\r";
+    bool bIsCmdProcess              = false;
 
+    bIsCmdProcess = cli_ProcessCmd(pcInputBuf, WRONG_CMD_SIZE, pcResult);
+    printf("%s", pcResult);
+    cli_ResetBuffer(pcResult);
+    printf("%s", pcResult);	
+    CHECK_EQUAL(true, bIsCmdProcess);
+}
 
 TEST(Module, cli_wrong_parameters_test)
 {
-	char pcInput[WRONG_PARAMETERS_CMD_SIZE] = "sum\r";
-	bool bIsCmdProcess                      = false;
-	
-	bIsCmdProcess = cli_ProcessCmd(pcInput, WRONG_PARAMETERS_CMD_SIZE, pcResultBuf);
-	printf("%s", pcResultBuf);
-	cli_ResetBuffer(pcResultBuf);
-	printf("%s", pcResultBuf);	
-	CHECK_EQUAL(true, bIsCmdProcess);
+    char pcInputBuf[WRONG_PARAMETERS_CMD_SIZE] = "sum\r";
+    bool bIsCmdProcess                         = false;
+
+    bIsCmdProcess = cli_ProcessCmd(pcInputBuf, WRONG_PARAMETERS_CMD_SIZE, pcResult);
+    printf("%s", pcResult);
+    cli_ResetBuffer(pcResult);
+    printf("%s", pcResult);	
+    CHECK_EQUAL(true, bIsCmdProcess);
 }
 
 TEST(Module, cli_null_cmd_test)
 {
-	char *pcInput       = NULL;
-	bool bIsCmdProcess  = false;
-	
-	bIsCmdProcess = cli_ProcessCmd(pcInput, WRONG_PARAMETERS_CMD_SIZE, pcResultBuf);
-	printf("%s", pcResultBuf);
-	cli_ResetBuffer(pcResultBuf);
-	printf("%s", pcResultBuf);	
-	CHECK_EQUAL(false, bIsCmdProcess);
+    char *pcInputBuf    = NULL;
+    bool bIsCmdProcess  = false;
+
+    bIsCmdProcess = cli_ProcessCmd(pcInputBuf, WRONG_PARAMETERS_CMD_SIZE, pcResult);
+    printf("%s", pcResult);
+    cli_ResetBuffer(pcResult);
+    printf("%s", pcResult);	
+    CHECK_EQUAL(false, bIsCmdProcess);
 }
